bf0efe8bf6f8a21620c8212c1a7e221237c9c8a3
DKowalski25 <dshinoda25@gmail.com>
2024-07-08 19:09:19 +0500

    refact

--- db/orm.py
+++ db/orm.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from settings import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME, LOG_ORM
 
-
 Base = declarative_base()
 
 engine = create_async_engine(
@@ -12,7 +11,6 @@ engine = create_async_engine(
     pool_size=100,
     max_overflow=0,
 )
-
 
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
