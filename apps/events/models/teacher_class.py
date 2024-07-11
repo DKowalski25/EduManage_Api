16fd6aa5871a303853873ac19553fba1a53add98
DKowalski25 <dshinoda25@gmail.com>
2024-07-11 16:30:18 +0500

    ED-4 -- Refactor

--- apps/events/models/teacher_class.py
+++ apps/events/models/teacher_class.py
@@ -1,8 +1,6 @@
-from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import String, Table, Column, ForeignKey
+from sqlalchemy import Table, Column, ForeignKey
 
 from db import Base
-
 
 teacher_class_association_table = Table(
     "teacher_class_association_table",
@@ -10,5 +8,3 @@ teacher_class_association_table = Table(
     Column('teacher_id', ForeignKey("teachers.id"), primary_key=True),
     Column('group_id', ForeignKey('groups.id'), primary_key=True)
 )
-
-
