295b5e7d3c3d0ecca40e12b5232e427df702e704
DKowalski25 <dshinoda25@gmail.com>
2024-07-11 17:29:27 +0500

    ED-4 -- Refact

--- apps/teachers/schemas/teacher.py
+++ apps/teachers/schemas/teacher.py
@@ -1,12 +1,46 @@
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 
-class Teacher(BaseModel):
+class TeacherClass(BaseModel):
+    group_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Task(BaseModel):
+    id: int
+    title: str
+    due_date: str
+
+    class Config:
+        orm_mode = True
+
+
+class FullTeacher(BaseModel):
     id: int
     first_name: str
     last_name: str
     email: EmailStr
     phone_number: int
     hashed_password: str
+    teacher_classes: list[TeacherClass] = []
+    tasks: list[Task] = []
 
     model_config = ConfigDict(from_attributes=True)
+
+    class Config:
+        orm_mode = True
+
+
+class Teacher(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: EmailStr
+
+    model_config = ConfigDict(from_attributes=True)
+
+    class Config:
+        orm_mode = True
+
