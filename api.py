0d7be395c009393f3a96761644027ff41ca6ce36
DKowalski25 <dshinoda25@gmail.com>
2024-07-16 20:11:30 +0500

    ED-5 -- Changed connection to FastApi

--- api.py
+++ api.py
@@ -1,10 +1,17 @@
 import uvicorn
+import settings
+from settings.development import HOT_RELOAD, LOG_LEVEL
+from core import logger, get_application  # noqa # pylint: disable=unused-import
 
-from fastapi import FastAPI
 
-from settings.common import HOST, PORT
+# use 'api:fast_api' to run using uvicorn/gunicorn like the folliwing:
+# gunicorn -w 4 -k uvicorn.workers.UvicornWorker api:fast_api
+# instead of using uvicorn/gunicorn directly in python like below
 
-app = FastAPI()
+fast_api = get_application()
 
-if __name__ == '__main__':
-    uvicorn.run(app, host=HOST, port=PORT)
+if __name__ == "__main__":
+    logger.info(f"Application is working on {settings.PORT} port\n")
+    uvicorn.run("api:fast_api", host=settings.HOST, port=settings.PORT,
+                log_level=LOG_LEVEL.lower(), reload=HOT_RELOAD)       # Was settings.LOG_LEVEL.lower()
+    # reload=settings.HOT_RELOAD
