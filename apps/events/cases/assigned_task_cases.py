97b724d9c101d88d2f666e5f038ac9c3c682dd57
DKowalski25 <dshinoda25@gmail.com>
2024-07-16 17:33:28 +0500

    ED-5 -- Refactor

--- apps/events/cases/assigned_task_cases.py
+++ apps/events/cases/assigned_task_cases.py
@@ -3,17 +3,17 @@ from ..schemas import AssignedTask, AssignedTaskCreate, AssignedTaskUpdate
 
 
 class AssignedTaskCases:
-    def __init__(self, assigned_task_storage: AssignedTaskStorage):
-        self.assigned_task_storage = assigned_task_storage
+    def __init__(self, assigned_task_repo: AssignedTaskStorage):
+        self.assigned_task_repo = assigned_task_repo
 
     async def create_assigned_task(self, assigned_task_create: AssignedTaskCreate) -> AssignedTask:
-        return await self.assigned_task_storage.create_assigned_task(assigned_task_create)
+        return await self.assigned_task_repo.create_assigned_task(assigned_task_create)
 
     async def get_all_assigned_tasks(self) -> list[AssignedTask]:
-        return await self.assigned_task_storage.get_all_assigned_tasks()
+        return await self.assigned_task_repo.get_all_assigned_tasks()
 
     async def get_assigned_task_by_id(self, assigned_task_id: int) -> AssignedTask:
-        return await self.assigned_task_storage.get_assigned_task_by_id(assigned_task_id)
+        return await self.assigned_task_repo.get_assigned_task_by_id(assigned_task_id)
 
     async def delete_assigned_task(self, assigned_task_id: int) -> AssignedTask | None:
-        return await self.assigned_task_storage.delete_assigned_task(assigned_task_id)
+        return await self.assigned_task_repo.delete_assigned_task(assigned_task_id)
--- apps/events/cases/assignment_cases.py
+++ apps/events/cases/assignment_cases.py
@@ -3,20 +3,20 @@ from ..schemas import Assignment, AssignmentCreate, AssignmentUpdate
 
 
 class AssignmentCases:
-    def __init__(self, assignment_storage: AssignmentStorage):
-        self.assignment_storage = assignment_storage
+    def __init__(self, assignment_repo: AssignmentStorage):
+        self.assignment_repo = assignment_repo
 
     async def create_assignment(self, assignment_create: AssignmentCreate) -> Assignment:
-        return await self.assignment_storage.create_assignment(assignment_create)
+        return await self.assignment_repo.create_assignment(assignment_create)
 
     async def get_all_assignments(self) -> list[Assignment]:
-        return await self.assignment_storage.get_all_assignments()
+        return await self.assignment_repo.get_all_assignments()
 
     async def get_assignment_by_id(self, assignment_id: int) -> Assignment:
-        return await self.assignment_storage.get_assignment_by_id(assignment_id)
+        return await self.assignment_repo.get_assignment_by_id(assignment_id)
 
     async def update_assignment(self, assignment_id: int, assignment_update: AssignmentUpdate) -> Assignment | None:
-        return await self.assignment_storage.update_assignment(assignment_id, assignment_update)
+        return await self.assignment_repo.update_assignment(assignment_id, assignment_update)
 
     async def delete_assignment(self, assignment_id: int) -> Assignment | None:
-        return await self.assignment_storage.delete_assignment(assignment_id)
+        return await self.assignment_repo.delete_assignment(assignment_id)
