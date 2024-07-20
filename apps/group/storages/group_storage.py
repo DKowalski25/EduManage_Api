d5aa66d7e0259757f1bce7c61305d704025bfe17
DKowalski25 <dshinoda25@gmail.com>
2024-07-20 18:28:04 +0500

    ED-5 -- Fixed the bug, now group_storage.py is right

--- apps/group/storages/group_storage.py
+++ apps/group/storages/group_storage.py
@@ -1,6 +1,6 @@
 from sqlalchemy.future import select
 
-from datetime import datetime
+from sqlalchemy.orm import selectinload
 
 from ..models import Group as GroupModel
 from ..schemas import Group, GroupCreate, GroupUpdate
@@ -14,43 +14,100 @@ class GroupStorage:
     @classmethod
     async def get_all_groups(cls) -> list[Group]:
         async with async_session() as session:
-            result = await session.execute(select(cls._table))
-            return [Group.model_validate(group) for group in result.scalars().all()]
+            # Request to get all users with related data download
+            stmt = select(cls._table).options(
+                selectinload(cls._table.teachers),
+                selectinload(cls._table.students),
+                selectinload(cls._table.assigned_tasks)
+            )
+            result = await session.execute(stmt)
+            groups = result.scalars().all()
+            return [Group.model_validate(group) for group in groups]
 
     @classmethod
-    async def get_group_by_id(cls, group_id: int) -> Group:
+    async def get_group_by_id(cls, group_id: int) -> Group | None:
         async with async_session() as session:
             result = await session.execute(select(cls._table).filter(cls._table.id == group_id))
             group = result.scalar()
-            return Group.model_validate(group) if group else None
+        async with async_session() as session:
+            # Request to get all users with related data download
+            stmt = select(cls._table).filter_by(id=group.id).options(
+                selectinload(cls._table.teachers),
+                selectinload(cls._table.students),
+                selectinload(cls._table.assigned_tasks)
+            )
+            result = await session.execute(stmt)
+            group = result.scalars().one_or_none()
+        return Group.model_validate(group) if group else None
 
     @classmethod
     async def create_group(cls, group_create: GroupCreate) -> Group:
         async with async_session() as session:
-            group = cls._table(**group_create.dict(), created_at=datetime.now())
+            group = cls._table(
+                group_name=group_create.group_name,
+                description=group_create.description,
+                grade=group_create.grade,
+            )
             session.add(group)
             await session.commit()
-            return Group.model_validate(group)
+            # Executing a request to get a newly created user
+        async with async_session() as session:
+            # Reading a created user using selectonload
+            stmt = select(cls._table).filter_by(id=group.id).options(
+                selectinload(cls._table.teachers),
+                selectinload(cls._table.students),
+                selectinload(cls._table.assigned_tasks)
+            )
+            result = await session.execute(stmt)
+            group = result.scalars().one_or_none()
+        return Group.model_validate(group)
 
     @classmethod
-    async def update_group(cls, user_id: int, group_update: GroupUpdate) -> Group | None:
+    async def update_group(cls, group_id: int, group_update: GroupUpdate) -> Group | None:
         async with async_session() as session:
-            result = await session.execute(select(cls._table).filter(cls._table.id == user_id))
-            group = result.scalar()
+            # Request for getting user with selectinload
+            stmt = select(cls._table).filter(cls._table.id == group_id).options(
+                selectinload(cls._table.teachers),
+                selectinload(cls._table.students),
+                selectinload(cls._table.assigned_tasks)
+            )
+            result = await session.execute(stmt)
+            group = result.scalar_one()
+
             if group:
+                # Updating user fields
                 for field, value in group_update.dict(exclude_unset=True).items():
                     setattr(group, field, value)
+
                 await session.commit()
-                return Group.model_validate(group)
-            return None
+            async with async_session() as session:
+                # Reading a created user using selectonload
+                stmt = select(cls._table).filter_by(id=group.id).options(
+                    selectinload(cls._table.teachers),
+                    selectinload(cls._table.students),
+                    selectinload(cls._table.assigned_tasks)
+                )
+                result = await session.execute(stmt)
+                group = result.scalars().one_or_none()
+            # Return updated user
+            return Group.model_validate(group)
 
     @classmethod
     async def delete_group(cls, group_id: int) -> Group | None:
         async with async_session() as session:
-            result = await session.execute(select(cls._table).filter(cls._table.id == group_id))
+            # Request for getting user with selectinload
+            stmt = select(cls._table).filter(cls._table.id == group_id).options(
+                selectinload(cls._table.teachers),
+                selectinload(cls._table.students),
+                selectinload(cls._table.assigned_tasks)
+            )
+            result = await session.execute(stmt)
             group = result.scalar()
+
             if group:
-                session.delete(group)
+                # Delete user
+                await session.delete(group)  # use await
                 await session.commit()
-                return Group.model_validate(group)
-            return None
+
+            # Return deleted user
+        return Group.model_validate(group)
