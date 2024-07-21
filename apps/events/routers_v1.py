33844e4940cce805d238277bb3259f5f1dcd8c6a
DKowalski25 <dshinoda25@gmail.com>
2024-07-21 17:37:15 +0500

    ED-5 -- Added assignment router

--- apps/events/routers_v1.py
+++ apps/events/routers_v1.py
@@ -1,6 +1,10 @@
 from .mark_router import router as mark_roure
+from .assignment_router import router as assignment_roure
 
-routers = ({"router": mark_roure, "extra_params": {"tags": ("marks", )}}, )
+routers = (
+    {"router": mark_roure, "extra_params": {"tags": ("marks", )}},
+    {"router": assignment_roure, "extra_params": {"tags": ("assignments", )}},
+)
 
 __all__ = (
     'routers',
