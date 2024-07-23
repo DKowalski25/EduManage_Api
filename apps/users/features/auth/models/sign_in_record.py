ff4e72026c3b1f5246bf9fe03087109d35b62285
DKowalski25 <dshinoda25@gmail.com>
2024-07-23 22:18:20 +0500

    ED-6 -- Created models

---
+++ apps/users/features/auth/models/sign_in_record.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, DateTime, ForeignKey
+from sqlalchemy.sql import func
+
+from db import Base
+
+
+class SignInRecord(Base):
+    __tablename__ = 'sign_in_records'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates="sign_in_records")
+    signed_in_at = Column(DateTime, server_default=func.now())
