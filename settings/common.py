944870039a306f22da0c82dda6af79ea6a12596b
DKowalski25 <dshinoda25@gmail.com>
2024-08-17 18:20:04 +0500

    ED-18 -- Added test_db var

--- settings/common.py
+++ settings/common.py
@@ -25,6 +25,13 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASS = os.environ.get("DB_PASS")
 DB_PORT = int(os.environ.get("DB_PORT"))
 
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
+DB_PORT_TEST = int(os.environ.get("DB_PORT_TEST"))
+
+
 LOGURU_BACKTRACE = True
 LOGURU_DIAGNOSE = True
 LOG_LEVEL = "INFO"
